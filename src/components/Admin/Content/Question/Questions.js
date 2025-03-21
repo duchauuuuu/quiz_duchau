ff01dd119c8d21a28a90be34eeed77d79acb185d
duchauuuuu <duchaunguyen131@gmail.com>
2025-03-21 12:58:41 +0700

    duchauuuuuu2103 git commit -m duchauuuuuu2103 git commit -m duchauuuuuu2103

--- src/components/Admin/Content/Question/Questions.js
+++ src/components/Admin/Content/Question/Questions.js
@@ -4,8 +4,9 @@ import "./Questions.scss";
 import { TiPlus } from "react-icons/ti";
 import { HiMinus } from "react-icons/hi";
 import { AiFillPicture } from "react-icons/ai";
-import { v4 as uuidv4 } from 'uuid';
-import _ from 'lodash';
+import { v4 as uuidv4 } from "uuid";
+import _ from "lodash";
+import Lightbox from "react-awesome-lightbox";
 const Questions = (props) => {
   const options = [
     { value: "chocolate", label: "chocolate" },
@@ -13,68 +14,123 @@ const Questions = (props) => {
     { value: "vanilla", label: "vanilla" },
   ];
   const [selectedQuiz, setSelectedQuiz] = useState({});
-  const[questions,setQuestions]=useState([
+  const [questions, setQuestions] = useState([
     {
-        id:
-        uuidv4(),
-        description:'',
-        imageFile:'',
-        imageName:'',
-        answers:[
-            {
-                id:uuidv4(),
-                description:'',
-                isCorrect:false
-            }
-        ]
+      id: uuidv4(),
+      description: "",
+      imageFile: "",
+      imageName: "",
+      answers: [
+        {
+          id: uuidv4(),
+          description: "",
+          isCorrect: false,
+        },
+      ],
+    },
+  ]);
+  const handleAddRemoveQuestion = (type, id) => {
+    if (type === "ADD") {
+      const newQuestions = {
+        id: uuidv4(),
+        description: "",
+        imageFile: "",
+        imageName: "",
+        answers: [
+          {
+            id: uuidv4(),
+            description: "",
+            isCorrect: false,
+          },
+        ],
+      };
+      setQuestions([...questions, newQuestions]);
+    } else if (type === "REMOVE") {
+      let questionsClone = _.cloneDeep(questions);
+      questionsClone = questionsClone.filter((item) => item.id !== id);
+      setQuestions(questionsClone);
     }
-  ])
-  const handleAddRemoveQuestion=(type,id)=>{
-        if(type==='ADD'){
-            const newQuestions= {
-                id:
-                uuidv4(),
-                description:'',
-                imageFile:'',
-                imageName:'',
-                answers:[
-                    {
-                        id:uuidv4(),
-                        description:'',
-                        isCorrect:false
-                    }
-                ]
-            }
-            setQuestions([...questions,newQuestions])
+  };
+
+  const handleAddRemoveAnswer = (type, questionId, answerId) => {
+    let questionsClone = _.cloneDeep(questions);
+    if (type === "ADD") {
+      const newAnswer = {
+        id: uuidv4(),
+        description: "",
+        isCorrect: false,
+      };
+
+      let index = questionsClone.findIndex((item) => item.id === questionId);
+      questionsClone[index].answers.push(newAnswer);
+      setQuestions(questionsClone);
+    } else if (type === "REMOVE") {
+      let index = questionsClone.findIndex((item) => item.id === questionId);
+      questionsClone[index].answers = questionsClone[index].answers.filter(
+        (item) => item.id !== answerId
+      );
+      setQuestions(questionsClone);
+    }
+  };
+  const handleOnChange = (type, questionId, value) => {
+    if ((type = "QUESTION")) {
+      let questionsClone = _.cloneDeep(questions);
+      let index = questionsClone.findIndex((item) => item.id === questionId);
+      if (index !== -1) {
+        questionsClone[index].description = value;
+        setQuestions(questionsClone);
+      }
+    }
+  };
+  const handleOnChangeFileQuestion = (questionId, event) => {
+    let questionsClone = _.cloneDeep(questions);
+    let index = questionsClone.findIndex((item) => item.id === questionId);
+    if (
+      index !== -1 &&
+      event.target &&
+      event.target.files &&
+      event.target.files[0]
+    ) {
+      questionsClone[index].imageFile = event.target.files[0];
+      questionsClone[index].imageName = event.target.files[0].name;
+      setQuestions(questionsClone);
+    }
+  };
+  const handleAnswerQuestion = (type, answerId, questionId, value) => {
+    let questionsClone = _.cloneDeep(questions);
+    let index = questionsClone.findIndex((item) => item.id === questionId);
+    if (index !== -1) {
+      questionsClone[index].answers.map((answer) => {
+        if (answer.id === answerId) {
+          if (type === "CHECKBOX") {
+            answer.isCorrect = value;
+          }
+          if (type === "INPUT") {
+            answer.description = value;
+          }
         }
-        else if(type==='REMOVE'){
-            let questionsClone= _.cloneDeep(questions);
-            questionsClone= questionsClone.filter(item=>item.id!==id)
-            setQuestions(questionsClone)    
-        }
+        return answer;
+      });
+      setQuestions(questionsClone);
+    }
+  };
+  const [isPreviewImage, setIsPreviewImage] = useState(false);
+  const handleSubmitQuestionForQuiz = () => {};
+  const [dataImagePreview,setDataImagePreview]= useState({
+    title: '',
+    url:''
+  })
+  const handlePreviewImage =(questionId) =>{
+    let questionsClone = _.cloneDeep(questions)
+    let index = questionsClone.findIndex((item) => item.id === questionId);
+    if(index!==-1){
+      setDataImagePreview({
+        url: URL.createObjectURL(questionsClone[index].imageFile),
+        title: questionsClone[index].imageName
+      })
+      setIsPreviewImage(true)
+    }
   }
- 
-  const handleAddRemoveAnswer=(type,questionId,answerId)=>{
-    let questionsClone=_.cloneDeep(questions);
-    if(type==='ADD'){
-        const newAnswer =
-            
-                {
-                    id:uuidv4(),
-                    description:'',
-                    isCorrect:false
-                }
-            
-        let index= questionsClone.findIndex(item=>item.id===questionId);
-        questionsClone[index].answers.push(newAnswer)
-        setQuestions(questionsClone)
-    }
-    else if(type==='REMOVE'){
-        let index= questionsClone.findIndex(item=>item.id===questionId);
-     questionsClone[index].answers= questionsClone[index].answers.filter(item=>item.id!==answerId) 
-     setQuestions(questionsClone)   
-    }
-}
   return (
     <div className="questions-container">
       <div className="title">Manage Questions</div>
@@ -89,69 +145,152 @@ const Questions = (props) => {
           />
         </div>
         <div className="mt-3 mb-2">Add new Question</div>
-        {
-            questions && questions.length>0 && questions.map((question,index)=>{
-                return(
-                    <div key={question.id} className="q-main mb-4">
-          <div className="questions-content">
-            <div className="form-floating description">
-              <input
-                type="type"
-                className="form-control"
-                placeholder="name@example.com"
-                value={question.description}
-              />
-              <label>Question {index+1} 's  description</label>
-            </div>
-            <div className="group-upload">
-              <label className="label-up">
-                <AiFillPicture />
-              </label>
-              <input type="file" hidden></input>
-              <span>0 file is uploaded</span>
-            </div>
-            <div className="btn-add">
-              <span onClick={()=>{handleAddRemoveQuestion('ADD','')}}>
-                <TiPlus className="icon-add" />
+        {questions &&
+          questions.length > 0 &&
+          questions.map((question, index) => {
+            return (
+              <div key={question.id} className="q-main mb-4">
+                <div className="questions-content">
+                  <div className="form-floating description">
+                    <input
+                      type="type"
+                      className="form-control"
+                      placeholder="name@example.com"
+                      value={question.description}
+                      onChange={(event) => {
+                        handleOnChange(
+                          "QUESTION",
+                          question.id,
+                          event.target.value
+                        );
+                      }}
+                    />
+                    <label>Question {index + 1} 's description</label>
+                  </div>
+                  <div className="group-upload">
+                    <label htmlFor={`${question.id}`}>
+                      <AiFillPicture className="label-up" />
+                    </label>
+                    <input
+                      id={`${question.id}`}
+                      type="file"
+                      hidden
+                      onChange={(event) =>
+                        handleOnChangeFileQuestion(question.id, event)
+                      }
+                    ></input>
+                    <span>
+                      {question.imageName ? (
+                        <span style={{cursor:"pointer"}} onClick={() => handlePreviewImage(question.id)}>
+                          
+                          question.imageName
+                        </span>
+                      ) : (
+                        "0 file is uploaded"
+                      )}
+                    </span>
+                  </div>
+                  <div className="btn-add">
+                    <span
+                      onClick={() => {
+                        handleAddRemoveQuestion("ADD", "");
+                      }}
+                    >
+                      <TiPlus className="icon-add" />
+                    </span>
+                    {questions.length > 1 && (
+                      <span
+                        onClick={() => {
+                          handleAddRemoveQuestion("REMOVE", question.id);
+                        }}
+                      >
+                        <HiMinus className="icon-remove" />
+                      </span>
+                    )}
+                  </div>
+                </div>
+                {question &&
+                  question.answers &&
+                  question.answers.length > 0 &&
+                  question.answers.map((answer, index) => {
+                    return (
+                      <div key={answer.id} className="answers-content">
+                        <input
+                          className="form-check-input iscorrect"
+                          type="checkbox"
+                          checked={answer.isCorrect}
+                          onChange={(event) =>
+                            handleAnswerQuestion(
+                              "CHECKBOX",
+                              answer.id,
+                              question.id,
+                              event.target.checked
+                            )
+                          }
+                        />
+                        <div className="form-floating answer-name">
+                          <input
+                            value={answer.description}
+                            type="type"
+                            className="form-control"
+                            placeholder=""
+                            onChange={(event) =>
+                              handleAnswerQuestion(
+                                "INPUT",
+                                answer.id,
+                                question.id,
+                                event.target.value
+                              )
+                            }
+                          />
+                          <label>Answers {index + 1}</label>
+                        </div>
+                        <div className="btn-group">
+                          <span
+                            onClick={() => {
+                              handleAddRemoveAnswer("ADD", question.id);
+                            }}
+                          >
+                            <TiPlus className="icon-add" />
+                          </span>
+                          {question.answers.length > 1 && (
+                            <span
+                              onClick={() => {
+                                handleAddRemoveAnswer(
+                                  "REMOVE",
+                                  question.id,
+                                  answer.id
+                                );
+                              }}
+                            >
+                              <HiMinus className="icon-remove" />
+                            </span>
+                          )}
+                        </div>
+                      </div>
+                    );
+                  })}
                 
-              </span>
-              {questions.length >1 &&
-              <span onClick={()=>{handleAddRemoveQuestion('REMOVE',question.id)}}>
-                <HiMinus className="icon-remove" />
-              </span>
-              }
-            </div>
+              </div>
+            );
+          })}
+        {questions && questions.length > 0 && (
+          <div className="btn-save">
+            <button
+              className="btn btn-warning"
+              onClick={() => handleSubmitQuestionForQuiz()}
+            >
+              Save Question
+            </button>
           </div>
-          {question && question.answers && question.answers.length>0 && question.answers.map((answer,index)=>{
-            return(
-                <div key={answer.id} className="answers-content">
-            <input className="form-check-input iscorrect" type="checkbox" />
-            <div className="form-floating answer-name">
-              <input value={answer.description} type="type" className="form-control" placeholder="" />
-              <label>Answers {index+1}</label>
-            </div>
-            <div className="btn-group">
-              <span onClick={()=>{handleAddRemoveAnswer('ADD',question.id)}} >
-                <TiPlus className="icon-add" />
-              </span>
-              { question.answers.length>1 && 
-              <span onClick={()=>{handleAddRemoveAnswer('REMOVE',question.id,answer.id)}} >
-                <HiMinus className="icon-remove" />
-              </span>}
-            </div>
-          </div>
-            )
-          })
-            
-          }
-       
-         
-
-        </div>
-                )
-            })
-        }
-     
+        )}
+        {isPreviewImage === true && (
+                  <Lightbox
+                    image={dataImagePreview.url}
+                    title={dataImagePreview.title}
+                    onClose={()=>setIsPreviewImage(false)}
+                  ></Lightbox>
+                )}
       </div>
     </div>
   );
